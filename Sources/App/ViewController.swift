c0ae1f561054a133b053f46fa47dbf3298803782
lauriane75 <lauriane.haydari@gmail.com>
2019-11-26 10:04:58 +0100

    Add storyboard

---
+++ Sources/App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTuto
+//
+//  Created by Lauriane Haydari on 26/11/2019.
+//  Copyright © 2019 Lauriane Haydari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
